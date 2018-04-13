4e112f8dda9d3a5f1a9d8150eb9a2dd4cec9a416
Nhan <nhan@codistry.io>
2018-04-12 23:24:25 -0700

    Complete loading images

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import _ from 'lodash';
 import './css/style.css';
+import dog from './img/dog.jpg';
 
 function component(){
   let element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  let dogPic = new Image();
+  dogPic.src = dog;
+
+  element.appendChild(dogPic);
 
   return element;
 }
